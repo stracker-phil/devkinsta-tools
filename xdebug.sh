457db2ba0f87b12e9294dea0965912732132c3e5
Philipp Stracker <stracker.phil@gmail.com>
2021-08-24 22:10:51 +0200

    Corretly disable/enable xdebug module

--- xdebug.sh
+++ xdebug.sh
@@ -1,33 +1,39 @@
 #!/bin/bash
 
-# Usage:
-# > docker exec devkinsta_fpm bash -c 'bash /www/kinsta/private/xdebug.sh on'
-# > docker exec devkinsta_fpm bash -c 'bash /www/kinsta/private/xdebug.sh off'
+#
+# Disables or enabled the Xdebug module for PHP.
+# Xdebug often decreases performance considerably. That's why it's best 
+# to disable it when it's not needed
+#
 
-# -----
-title() {
-  echo; echo "=== $1 ==="
+source "$(dirname $0)/.lib.sh"
+
+usage() {
+	title "Usage"
+	cmd "$0" "on"
+	cmd "$0" "off"
+	exit 1
 }
-# -----
+
+if [ -z "$1" ]; then
+	usage
+fi
+
+# Propagate the command to docker, if called on the host.
+run_in_docker $0 $*
 
 if [ "on" == "$1" ] || [ "enable" == "$1" ]; then
 	state=on
-else 
+else
 	state=off
 fi
 
-update-alternatives --list php | while read bin ; do
-	version=${bin#"/usr/bin/php"}
-	ini_path="/etc/php/$version/fpm/conf.d/20-xdebug.ini"
-	service="/etc/init.d/php$version-fpm"
+if [ "on" == "$state" ]; then	
+	title "Enable Xdebug"
+	phpenmod xdebug
+else
+	title "Disable Xdebug"
+	phpdismod xdebug
+fi
 
-	if [ "on" == "$state" ]; then	
-		title "Enable Xdebug"; sed -ie 's/^; //' $ini_path
-	else
-		title "Disable Xdebug"; sed -ie 's/^/; /' $ini_path
-	fi 
-
-	title "Restart $service"; $service restart
-done
-
-echo; echo "All done"
+log "All done"
